e83ba5e63cf1515d813b486a56317e020e5d3e74
Tuguldur Baigalmaa <philoniare@gmail.com>
2017-06-06 02:50:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+Handlebars.registerHelper('comment_body', function() {
+  if(this.state === "closed") {
+    return new Handlebars.SafeString(this.body)
+  } else {
+    return new Handlebars.SafeString("<strong>" + this.body + "</strong>")
+  }
+})
 
+function loadIssues() {
+  var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
+  var result = template(issues);
+  document.getElementsByTagName("main")[0].innerHTML += result;
+}
