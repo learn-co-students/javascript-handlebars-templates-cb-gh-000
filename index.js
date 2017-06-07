fd11be0b567130f984cce16a007b447a990fd891
Robert Wayne <robertjwayne@gmail.com>
2017-06-07 11:16:15 -0400

    Add comment_body helper to make open comments bold

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+Handlebars.registerHelper("comment_body", function() {
+  if (this.state === "closed") {
+    return new Handlebars.SafeString(this.body);
+  } else {
+    return new Handlebars.SafeString("<strong>" + this.body + "</strong>");
+  }
+});
+
 function loadIssues() {
   var issue = {
     state: "closed",
