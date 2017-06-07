1105888b1e25f66e7b299f7f699a68594d5184bd
Robert Wayne <robertjwayne@gmail.com>
2017-06-07 10:59:27 -0400

    Add loadIssue function

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+function loadIssue() {
+  var issue = {
+    state: "closed",
+    number: 5,
+    created_at: "2016-03-31 16:23:13 UTC",
+    body: "Instructions say GET /team and POST /newteam. Rspec wants GET/newteam and POST/team.",
+  };
 
+  var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
+  var result = template(issue);
+  document.getElementsByTagName("main")[0].innerHTML += result;
+}
